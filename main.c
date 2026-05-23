210e5b7751be3049a2a8f5e7e8813edcb0599fc1
kiyah7897 <raraj0404@gmail.com>
2026-05-22 22:22:59 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cache.h"
+
+
+int main(int argc, char** argv) {
+    // keep a counter of cache latency
+    int total_latency = 0;
+
+    // print out total latency at the end
+    printf("Total latency=%d\n", total_latency);
+
+    // Make sure to exit cleanly when you fix this code
+    abort();
+}
